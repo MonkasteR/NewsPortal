02c5bbf9ac0d08f38c4e6b74d5a87a63f8451fbf
MonkasteR <monkaster@gmail.com>
2023-08-08 00:23:58 +0300

    Подправил не запускается

--- NewsPortal/settings.py
+++ NewsPortal/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.flatpages',
     'News.apps.NewsConfig',
     'django.contrib.sites.models.Site',
 ]
