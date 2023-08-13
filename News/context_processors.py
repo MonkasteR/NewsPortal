17cdea94aa57af2f242b8ca05754f460473db7d9
MonkasteR <monkaster@gmail.com>
2023-08-13 22:17:22 +0300

    Удаление новости. Работает Редактирование новости. Работает Тайтлы отображаются

--- News/context_processors.py
+++ News/context_processors.py
@@ -5,3 +5,10 @@ def news_for_header(request):
     return {
         'news_for_header': Post.objects.order_by('-dateCreation')
     }
+
+
+def news_pk(request):
+    return {
+        'news_pk': Post.objects.get(pk=request)
+        # 'news_pk': Post.objects.get(pk=request.form.get('pk'))
+    }
