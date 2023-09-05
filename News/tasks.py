d2af66d38210c674f41101150e4823e376f841d4
MonkasteR <monkaster@gmail.com>
2023-09-05 22:22:13 +0300

    Рассылка почты по созданию новости и еженедельная рассылка работают через Celery

--- News/tasks.py
+++ News/tasks.py
@@ -1,4 +1,5 @@
-from datetime import datetime
+# from datetime import datetime
+import datetime
 
 from celery import shared_task
 from django.core.mail import EmailMultiAlternatives
--- NewsPortal/celery.py
+++ NewsPortal/celery.py
@@ -10,7 +10,7 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 
 app.conf.beat_schedule = {
     'action_every_monday_8am': {
-        'task': 'my_task',
+        'task': 'News.tasks.my_task',
         'schedule': crontab(hour=8, minute=0, day_of_week='monday'),
         # 'args': (args),
     },
