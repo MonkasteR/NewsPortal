766bfdcfb94c2ce9b8430d4d2a545fc4797c74b7
MonkasteR <monkaster@gmail.com>
2023-09-07 21:50:59 +0300

    Настроил кэширование страниц и темплейтов.

--- News/urls.py
+++ News/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
+from django.views.decorators.cache import cache_page
 
 from accounts.views import test_page
 from .views import PostList, PostDetail, NewsCreate, NewsUpdate, NewsDelete, CategoryListView, subscriptions, subscribe
 
 urlpatterns = [
-    path('', PostList.as_view(), name='posts_list'),
-    path('<int:pk>', PostDetail.as_view(), name='one_news'),
+    path('', cache_page(60)(PostList.as_view()), name='posts_list'),
+    path('<int:pk>', cache_page(300)(PostDetail.as_view()), name='one_news'),
     path('search/', PostList.as_view()),
     path('create/', NewsCreate.as_view(), name='news_create'),
     path('<int:pk>/edit/', NewsUpdate.as_view(), name='news_update'),
