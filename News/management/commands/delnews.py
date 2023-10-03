9aa3d5fa53417c12472bc62c1a92036779a4b2f1
MonkasteR <monkaster@gmail.com>
2023-10-03 15:10:05 +0300

    Удаление новостей и обновление зависимостей

---
+++ News/management/commands/delnews.py
@@ -0,0 +1,47 @@
+from django.core.management.base import BaseCommand
+
+from News.models import Category, Post
+
+
+class Command(BaseCommand):
+    help = 'Удаление статей в определенной категории'
+
+    def add_arguments(self, parser):
+        """
+        Добавляет указанные аргументы командной строки в объект парсера.
+
+        Параметры:
+        - `parser`: Объект парсера, в который добавляются аргументы.
+
+        Возвращаемое значение:
+        Нет
+        """
+        parser.add_argument('category', type=str)
+
+    def handle(self, *args, **options):
+        """
+        Обрабатывает удаление всех статей в указанной категории.
+
+        Параметры:
+        - `args`: Список аргументов переменной длины.
+        - `options`: Произвольные именованные аргументы.
+
+        Возвращаемое значение:
+        Нет
+        """
+        category_name = options.get('category')
+
+        if category_name:
+            answer = input(f'Вы правда хотите удалить все статьи в категории {category_name}? (yes/no): ')
+
+            if answer.lower() == 'yes':
+                try:
+                    category = Category.objects.get(name=category_name)
+                    Post.objects.filter(postCategory=category).delete()
+                    self.stdout.write(self.style.SUCCESS(f'Все статьи в категории {category_name} удалены'))
+                except Category.DoesNotExist:
+                    self.stdout.write(self.style.ERROR(f'Не найдено категории {category_name}'))
+            else:
+                self.stdout.write(self.style.ERROR('Отменено'))
+        else:
+            self.stdout.write(self.style.ERROR('Не указана категория'))
